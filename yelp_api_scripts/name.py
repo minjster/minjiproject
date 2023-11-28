7413483f7613175d4fc3552bbb0c2b2453c918f2
minjster <lminji735@gmail.com>
2023-11-27 23:33:13 -0500

    first api request

--- yelp_api_scripts/name.py
+++ yelp_api_scripts/name.py
@@ -1,2 +1,20 @@
 import pandas as pd
-print("done")
+import requests
+
+API_KEY_FILEPATH = "/Users/minji/Desktop/secrets/yelp_api_key.txt"
+
+url = "https://api.yelp.com/v3/businesses/search"
+key = open(rf"{API_KEY_FILEPATH}").readlines()[0]
+headers = {"Authorization": f"Bearer {key}"}
+print(key)
+
+parameters = {
+    "location": "12880 Riverplace Court, Jacksonville, FL",
+    "categories": "coffee",
+    "radius": 10000,
+    "limit": 3,
+}
+
+response = requests.get(url, headers=headers, params=parameters)
+
+print(response.json()["businesses"])
